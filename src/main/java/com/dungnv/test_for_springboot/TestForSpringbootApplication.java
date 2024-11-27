bc6c5fd161aac6f8e1075f83e581bdc4b567fc40
nguyendungnd204 <125289761+nguyendungnd204@users.noreply.github.com>
2024-11-28 04:54:23 +0700

    Initial commit

---
+++ src/main/java/com/dungnv/test_for_springboot/TestForSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.dungnv.test_for_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestForSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestForSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dungnv/test_for_springboot/TestForSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dungnv.test_for_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestForSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
